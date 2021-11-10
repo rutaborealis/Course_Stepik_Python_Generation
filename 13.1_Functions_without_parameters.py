bf3d1983e19772bcbb8adf2682ccb93066fd874e
rutaborealis <ruta.kukharenko@gmail.com>
2021-11-10 23:11:07 +0300

    2 files added: 13.1 - 13.2

---
+++ 13.1_Functions_without_parameters.py
@@ -0,0 +1,19 @@
+# 13.1
+# 13.1.1
+def draw_box():
+    print('*' * 10)
+    for i in range(12):
+        print('*' + ' ' * 8 + '*')
+    print('*' * 10)
+
+
+draw_box()
+
+
+# 13.1.1
+def draw_triangle():
+    for i in range(1, 11):
+        print('*' * i)
+
+
+draw_triangle()
